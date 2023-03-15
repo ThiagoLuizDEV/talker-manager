a4def725ac151ce3368999a1841afc6834e4df83
Thiago <thiago-470@outlook.com>
2023-03-15 17:59:55 -0300

    req05

--- src/fsFunctions.js
+++ src/fsFunctions.js
@@ -37,7 +37,7 @@ const randomTalker = () => {
 
 const validationEmail = (req, res, next) => {
     const { email } = req.body;
-    const validation = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+    const validation = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i;
 
     if (!email) {
         return res.status(400).json({ message: 'O campo "email" é obrigatório' });
@@ -63,10 +63,97 @@ const validationPassword = (req, res, next) => {
     next();
 };
 
+const validationToken = (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        return res.status(401).json({ message: 'Token não encontrado' });
+    }
+    if (authorization.length !== 16) {
+        return res.status(401).json({ message: 'Token inválido' });
+    }
+
+    next();
+};
+
+const validationName = (req, res, next) => {
+    const { name } = req.body;
+
+    if (!name) {
+        return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+    }
+    if (name.length < 3) {
+        return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+    }
+    next();
+};
+
+const validationAge = (req, res, next) => {
+    const { age } = req.body;
+    if (!age) {
+        return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+    }
+    if (age < 18 || !Number.isInteger(age)) {
+        return res.status(400).json(
+            { message: 'O campo "age" deve ser um número inteiro igual ou maior que 18' },
+);
+    }
+    next();
+};
+
+const validationTalk = (req, res, next) => {
+    const { talk } = req.body;
+
+    if (!talk) {
+        return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+    }
+
+    next();
+};
+
+const validationWatched = (req, res, next) => {
+    const { watchedAt } = req.body.talk;
+
+    const regexDate = /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[0-2])\/(20[0-9]{2}|[3-9][0-9])$/i;
+
+    if (!watchedAt) {
+        return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+    }
+
+    if (!regexDate.test(watchedAt)) {
+        return res.status(400)
+        .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+    }
+    next();
+};
+
+const validationNumber = (num) => !Number.isInteger(num) || (num < 1 || num > 5);
+
+const validationRate = (req, res, next) => {
+    const { rate } = req.body.talk;
+
+    if (!rate && rate !== 0) {
+        return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+    }
+    if (validationNumber(rate)) {
+        return res.status(400)
+        .json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });
+    }
+    next();
+};
+
 module.exports = {
     allTalkers,
     talkerID,
     randomTalker,
     validationEmail,
     validationPassword,
+    validationToken,
+    validationName,
+    validationAge,
+    validationTalk,
+    validationWatched,
+    validationRate,
+    completePath,
+    fs,
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const fsFunctions = require('./fsFunctions');
 
-const { validationEmail } = fsFunctions;
-
-const { validationPassword } = fsFunctions;
+const { validationEmail,
+  validationPassword,
+  validationToken,
+  validationName,
+  validationAge,
+  validationTalk,
+  validationWatched,
+  validationRate } = fsFunctions;
 
 const app = express();
 app.use(express.json());
@@ -34,6 +39,31 @@ app.get('/talker/:id', async (req, res) => {
 app.post('/login', validationPassword, validationEmail, (_req, res) => {
   const result = fsFunctions.randomTalker();
   return res.status(200).json({ token: result });
+});
+
+app.post('/talker',
+validationToken,
+validationName,
+validationAge,
+validationTalk,
+validationWatched,
+validationRate,
+async (req, res) => {
+  const { name, age, talk: { watchedAt, rate } } = req.body;
+  const result = await fsFunctions.allTalkers();
+
+  const futureTalk = {
+    name,
+    age,
+    id: result.length += 1,
+    talk: {
+      watchedAt,
+      rate,
+    },
+  };
+  const talkers = JSON.stringify([futureTalk]);
+  await fsFunctions.fs.writeFile(fsFunctions.completePath, talkers);
+  return res.status(201).json(futureTalk);
 });
 
 app.listen(PORT, () => {
